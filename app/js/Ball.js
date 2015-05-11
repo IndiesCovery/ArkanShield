e5370cf5617314692e2b1d81ec7522898b18f778
José Miguel <jmromeroe@gmail.com>
2015-05-10 20:42:48 -0500

    Some corrections added

--- app/js/Ball.js
+++ app/js/Ball.js
@@ -1,8 +1,7 @@
 Ball = function(game, x, y, key){
   Phaser.Sprite.call(this, game, x, y, key);
   this.speed = {};
-  this.speed.y = 400;
-  this.speed.x = 100;
+  this.speed.y = 350;
   this.anchor.set(0.5);
 }
 
