8b5757c680358163b632d47b827fbda76896b8a2
agares <ekeisco@gmail.com>
2015-05-13 01:41:48 -0500

    refactor ages into one single class Age

---
+++ app/js/Age.js
@@ -0,0 +1,36 @@
+Age = function(theme){
+  this.bricks = game.add.group();
+  this.bricks.enableBody = true;
+  this.bricks.physicsBodyType = Phaser.Physics.ARCADE;
+  this.done = false;
+  this.fillBricksList(theme.spriteSet, theme.frames);
+};
+
+Age.prototype.constructor = Age;
+
+Age.prototype.fillBricksList = function(theme, frames){
+  limits = {
+    top: 0,     // regular 0
+    bottom: 6,  // regular 6
+    left: 0,    // regular 0
+    right: 14   // regular 14
+  };
+  
+  for(var i = limits.left; i < limits.right; i++){  
+    for(var j = limits.top; j < limits.bottom; j++){      
+      var brick = new Brick(game, (i+1)*50, (j+1)*50, theme+(Math.floor(Math.random()*frames+1)));
+      this.bricks.add(brick);
+      brick.body.immovable = true;
+      brick.body.bounce.set(0);
+    }
+  }
+}
+
+Age.prototype.hitBrickBall  = function(_ball, _brick){
+  _brick.kill();
+}
+
+Age.prototype.update = function(){
+  if(this.bricks.countLiving() == 0) this.done = true;
+  game.physics.arcade.collide(ball, this.bricks, this.hitBrickBall, null, this);
+}
