0687cb293885e77ce2236f5374491ed4421c7dd3
agares <ekeisco@gmail.com>
2015-05-13 01:25:47 -0500

    fixing format

--- js/Ages/Age.js
+++ js/Ages/Age.js
@@ -1,17 +1,20 @@
-Future = function(){
+Age = function(theme){
   this.bricks = game.add.group();
   this.bricks.enableBody = true;
   this.bricks.physicsBodyType = Phaser.Physics.ARCADE;
   this.done = false;
 
-  this.fillBricksList();
-}
+  this.fillBricksList(theme);
+};
 
-Future.prototype.fillBricksList = function(){
+Age.prototype = Object.create();
+Age.prototype.constructor = Age;
+
+Age.prototype.fillBricksList = function(theme){
 
     for(var i = 0; i < 14; i++){  
       for(var j = 0; j < 6; j++){      
-        var brick = new Brick(game, (i+1)*50, (j+1)*50, 'brickFuture'+(Math.floor(Math.random()*3+1)));
+        var brick = new Brick(game, (i+1)*50, (j+1)*50, theme+(Math.floor(Math.random()*5+1)));
         this.bricks.add(brick);
         brick.body.immovable = true;
         brick.body.bounce.set(0);
@@ -19,11 +22,11 @@ Future.prototype.fillBricksList = function(){
     }
 }
 
-Future.prototype.hitBrickBall  = function(_ball, _brick){
+Age.prototype.hitBrickBall  = function(_ball, _brick){
   _brick.kill();
 }
 
-Future.prototype.update = function(){
+Age.prototype.update = function(){
   if(this.bricks.countLiving() == 0) this.done = true;
   game.physics.arcade.collide(ball, this.bricks, this.hitBrickBall, null, this);
 }
--- js/Ages/Stone.js
+++ js/Ages/Stone.js
@@ -1,30 +1,29 @@
 Stone = function(){
+  this.bricks = game.add.group();
+  this.bricks.enableBody = true;
+  this.bricks.physicsBodyType = Phaser.Physics.ARCADE;
+  this.done = false;
 
-	this.bricks = game.add.group();
-	this.bricks.enableBody = true;
-	this.bricks.physicsBodyType = Phaser.Physics.ARCADE;
-	this.done = false;
-
-	this.fillBricksList();
+  this.fillBricksList();
 }
 
 Stone.prototype.fillBricksList = function(){
 
-		for(var i = 0; i < 14; i++){	
-			for(var j = 0; j < 6; j++){ 		 
-		 		 var brick = new Brick(game, (i+1)*50, (j+1)*50, 'brickStone'+(Math.floor(Math.random()*4+1)));
-		 		 this.bricks.add(brick);
-	 			 brick.body.immovable = true;
-		 		 brick.body.bounce.set(0);	
-	 		 } 
- 		}
+    for(var i = 0; i < 14; i++){  
+      for(var j = 0; j < 6; j++){      
+        var brick = new Brick(game, (i+1)*50, (j+1)*50, 'brickStone'+(Math.floor(Math.random()*4+1)));
+        this.bricks.add(brick);
+        brick.body.immovable = true;
+        brick.body.bounce.set(0);  
+       } 
+    }
 }
 
 Stone.prototype.hitBrickBall  = function(_ball, _brick){
-	_brick.kill();
+  _brick.kill();
 }
 
 Stone.prototype.update = function(){
-	if(this.bricks.countLiving() == 0) this.done = true;
-	game.physics.arcade.collide(ball, this.bricks, this.hitBrickBall, null, this);
+  if(this.bricks.countLiving() == 0) this.done = true;
+  game.physics.arcade.collide(ball, this.bricks, this.hitBrickBall, null, this);
 }
