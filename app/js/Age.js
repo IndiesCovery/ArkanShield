a8a51ec51968218935880b9ce04eb5a433becc07
Agares <ekeisco@gmail.com>
2015-05-13 14:49:26 -0500

    changes bounds

--- app/js/Age.js
+++ app/js/Age.js
@@ -10,15 +10,15 @@ Age.prototype.constructor = Age;
 
 Age.prototype.fillBricksList = function(theme, frames){
   limits = {
-    top: 0,     // regular 0
-    bottom: 6,  // regular 6
-    left: 0,    // regular 0
-    right: 14   // regular 14
+    top: 3,     // regular 0
+    bottom: 23,  // regular 6
+    left: 1,    // regular 0
+    right: 21   // regular 14
   };
   
   for(var i = limits.left; i < limits.right; i++){  
     for(var j = limits.top; j < limits.bottom; j++){      
-      var brick = new Brick(game, (i+1)*50, (j+1)*50, theme+(Math.floor(Math.random()*frames+1)));
+      var brick = new Brick(game, (i+1)*34, (j+1)*18, theme+(Math.floor(Math.random()*frames+1)));
       this.bricks.add(brick);
       brick.body.immovable = true;
       brick.body.bounce.set(0);
